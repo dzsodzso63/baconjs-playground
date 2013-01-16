e40f3d4c87462978dbb684e0be3e8c153fd61945
Dzso Pengo <dzsodzso63@gmail.com>
2013-01-16 18:55:12 +0100

    Show zebra on object click

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+
 $(function() {
-    var clicks = $("#b_object").asEventStream("click");
-    clicks.log();
+    var object_click = $("#b_object").asEventStream("click").map(function(event){return $("#b_object");});
+    var mouse_position = $(document).asEventStream("mousemove").map(function(event) { return {x: event.clientX, y: event.clientY}; }).toProperty({x: 0, y: 0});
+    var zebra_visible = function(){return $("#b_zebra").is(':visible');};
+    var show_zebra = object_click.filter(function(){return !zebra_visible();});
+    show_zebra.onValue(function(object){
+        centerObjectToObject($("#b_zebra"), object);
+        $("#b_zebra").fadeIn(100);
+    });
 });
+
+function centerObjectToObject(objectToCenter, objectBase){
+    var center = {
+        x: objectBase.offset().left + (objectBase.width()  / 2),
+        y: objectBase.offset().top  + (objectBase.height() / 2)
+    };
+    console.log(objectBase.width(), center);
+    objectToCenter.css("left", center.x - objectToCenter.width() / 2)
+        .css("top", center.y - objectToCenter.height() / 2);
+}
